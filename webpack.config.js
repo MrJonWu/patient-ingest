ccfec790a8d3846e2ae48b9227c8810366d3f7e1
MrJonWu <mrjonwu@gmail.com>
2017-02-22 23:34:12 -0800

    Webpack parsing issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
-
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    path: './src/',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
     ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+  }
 };
