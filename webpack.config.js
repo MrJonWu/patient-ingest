1e759baa44e5e48997f4fc11e52d00030e7c54f6
MrJonWu <mrjonwu@gmail.com>
2017-02-27 13:36:38 -0800

    optimized bundle.js size

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const webpack = require('webpack');
+
 module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: './src/index.jsx',
   output: {
     path: './src',
@@ -15,5 +18,14 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
 };
